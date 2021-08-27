f7376a5f25944891405d35ad6f78691791382fa7
Kaijonsson <axel.jonsson@hotmail.com>
2021-08-27 09:17:13 +0200

    foundation done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Home from "./views/Home";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// API :: https://api.spaceflightnewsapi.net/v3/documentation#/Article/
+// FIRST PAGE :: TABLE LISTING LASTEST NEWS MAX 10 ARTICLES
+// PAGINATE FOR NEXT 10
+// NEWEST FIRST
+// ALSO SHOW DATE, TITLE
+// SEARCH ARTICLES BASED ON STRING - IMPLEMENT "DEBOUNCE" - AVOID FLOODING
+
+// ARTICLE SELECTED - > NEW PAGE = SUMMARY, IMAGE, DATE, TITLE, LINK TO ACTUAL ARTICLE.
+
+/*
+
+*/
---
+++ src/views/SummarizedNews.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SummarizedNews() {
+  return <div></div>;
+}
+
+export default SummarizedNews;
