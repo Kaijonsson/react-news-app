8e2a712e9675f67198480ee4a7cb50b73e04d8e7
Kaijonsson <axel.jonsson@hotmail.com>
2021-11-03 09:43:42 +0100

    server config

--- server/index.js
+++ server/index.js
@@ -4,12 +4,4 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.listen(3000, () => console.log("listening to 3000"));
-
-/**
- 
- C:\Users\Axelj\Visual-Studio\arbetsprov\pinpoint\react-app
- 
- C:\Users\Axelj\Visual-Studio\arbetsprov\pinpoint\react-app\build\static\css
-
- */
+app.listen(3010, () => console.log("listening to 3010"));
